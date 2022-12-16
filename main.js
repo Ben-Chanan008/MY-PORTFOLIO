9b18ca9a944029c308576b1e622967908c8384f7
Great Ben <bchanan.boss@gmail.com>
2022-12-16 13:34:17 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const hoverLi = document.querySelector('.js');
+const hoverLi2 = document.querySelector('.htmlandcss');
+const hoverLi3 = document.querySelector('.react');
+const table = document.querySelector('.table');
+
+function tables(){
+    table.innerHTML = "<ul><li>SLIDER</li> <li>CALCULATOR</li></ul>";
+    table.classList.add('.tables');
+}
+
+hoverLi.addEventListener('click', (tables));
